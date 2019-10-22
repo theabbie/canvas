7573387afa901cca366c2418509dcb1696846091
abhishek <abhishek7gg7@gmail.com>
2019-10-22 14:48:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 var app = require('express')();
-const { createCanvas, loadImage } = require('canvas');
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+const axios = require("axios");
+const $ = require("cheerio");
+const fs = require("fs");
 
-app.get("/*", function(req,res) {
-const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d')
-
-// Write "Awesome!"
-ctx.font = '30px Impact'
-ctx.rotate(0.1)
-ctx.fillText('Awesome!', 50, 100)
-
-// Draw line under text
-var text = ctx.measureText('Awesome!')
-ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-ctx.beginPath()
-ctx.lineTo(50, 102)
-ctx.lineTo(50 + text.width, 102)
-ctx.stroke()
-res.end(canvas.toDataURL());
+app.get("/*", async function(req,res) {
+try {
+    const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+    });
+   const page = browser.newPage();
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+    await page.setViewport({
+      'width': 375,
+      'height': 812,
+      'deviceScaleFactor': 3,
+      'isMobile': true,
+      'hasTouch': true,
+      'isLandscape': false
+    });
+    await page.goto("https://google.com/");
+    res.end(await page.screenshot());
+    await browser.close();
+}
+catch(err) {
+    res.send(err.message);
+   }
 })
 
 app.listen(process.env.PORT);
