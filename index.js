f8a908a039a09d4a03b4e25ad9fa7ee1967697c6
abhishek <abhishek7gg7@gmail.com>
2019-10-22 14:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ try {
         headless: chrome.headless
     });
    const page = await browser.newPage();
-    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
-  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
-    await page.setUserAgent(userAgent);
+   await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36');
     await page.setViewport({
       'width': 375,
       'height': 812,
@@ -24,7 +22,7 @@ try {
       'hasTouch': true,
       'isLandscape': false
     });
-    await page.goto("https://google.com/");
+    await page.goto(`data:text/html,<h1>Hello World</h1>`);
     res.end(await page.screenshot());
     await browser.close();
 }
